5e19c6032eb9be813179eb3c2e59ffbaa24fec90
HBlackfoxx <hamada.cero@gmail.com>
2025-02-27 19:00:45 +0100

    S

--- src/app/home/gallery/gallery.component.ts
+++ src/app/home/gallery/gallery.component.ts
@@ -9,18 +9,23 @@ import { Component } from '@angular/core';
 export class GalleryComponent {
   currentIndex = 0;
   carouselImages = [
-    'assets/images/1.jpg',
-    'assets/images/2.jpg',
-    'assets/images/3.jpg',
-    'assets/images/4.jpg',
-    'assets/images/5.jpg',
-    'assets/images/6.jpg',
-    'assets/images/7.jpg',
-    'assets/images/8.jpg',
-    'assets/images/9.jpg',
-    'assets/images/10.jpg',
-    'assets/images/11.jpg',
-    'assets/images/12.jpg',
+    'assets/images/gallery/1.jpg',
+    'assets/images/gallery/2.jpg',
+    'assets/images/gallery/3.jpg',
+    'assets/images/gallery/4.jpg',
+    'assets/images/gallery/5.jpg',
+    'assets/images/gallery/6.jpg',
+    'assets/images/gallery/7.jpg',
+    'assets/images/gallery/8.jpg',
+    'assets/images/gallery/9.jpg',
+    'assets/images/gallery/10.jpg',
+    'assets/images/gallery/11.jpg',
+    'assets/images/gallery/12.jpg',
+    'assets/images/gallery/13.png',
+    'assets/images/gallery/14.jpg',
+    'assets/images/gallery/15.png',
+    'assets/images/gallery/16.jpg',
+    'assets/images/gallery/17.jpg',
   ];
 
   moveTo(direction: 'prev' | 'next'): void {
