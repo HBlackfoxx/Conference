e7f2408fd46af90e850915bbb693ae308d844325
HBlackfoxx <hamada.cero@gmail.com>
2025-03-12 00:56:14 +0100

    commit

--- src/app/home/speakers/speakers.component.ts
+++ src/app/home/speakers/speakers.component.ts
@@ -7,10 +7,15 @@ import { Component } from '@angular/core';
 })
 export class SpeakersComponent {
   speakers = [
-    { name: 'Coming Soon', 
-      role: '', 
-      imageUrl: '', 
-      description: ''
+    { name: 'Dr. Jay Daniel', 
+      role: 'Professor of Digital Supply Chain and Technology Innovation | Reasearch Cluster Lead at thee Center for Supply CHain Improvement | Derby Business School, University of Derby, United Kingdom', 
+      imageUrl: 'assets/images/speakers/speaker1.jpg', 
+      description: 'Talk: Digital Supply Chain and Blockchain Technology: How to Unblock the Chain'
+    },
+    { name: 'Dr. Guilherme F.Frederico', 
+      role: 'Professor of Operations, Supply Chain and Project Management at Federal University of Parana - UFPR - School of Management, Curitiba, Brazil', 
+      imageUrl: 'assets/images/speakers/Speaker2.jpg', 
+      description: 'Talk: Supply Chain 4.0: An strategic approach'
     },
   ];
 }
