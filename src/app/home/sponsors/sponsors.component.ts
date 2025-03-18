883bb56707bc1673e253691f30a93aaa85b2085b
HBlackfoxx <hamada.cero@gmail.com>
2025-03-18 14:58:00 +0100

    push

--- src/app/home/sponsors/sponsors.component.ts
+++ src/app/home/sponsors/sponsors.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// sponsors.component.ts
+import { Component, OnInit } from '@angular/core';
 
 interface Sponsor {
   name: string;
@@ -9,15 +10,26 @@ interface Sponsor {
 @Component({
   selector: 'app-sponsors',
   templateUrl: './sponsors.component.html',
-  styleUrl: './sponsors.component.css'
+  styleUrls: ['./sponsors.component.css']
 })
-export class SponsorsComponent {
+export class SponsorsComponent implements OnInit {
   sponsors: Sponsor[] = [
-    { name: 'Sponsor 1', imageUrl: '/assets/images/partners/springer.webp', link: 'https://www.springer.com/' },
-    { name: 'Sponsor 2', imageUrl: '/assets/images/partners/ensa.webp', link: 'https://ensa-marrakech.uca.ma/' },
-    { name: 'Sponsor 3', imageUrl: '/assets/images/partners/aminov.webp', link: '#'},
-    { name: 'Sponsor 4', imageUrl: '/assets/images/partners/Lartid.jpg', link: 'https://ensa-marrakech.uca.ma/' },
-    { name: 'Sponsor 5', imageUrl: '/assets/images/partners/Blockhat.png', link: 'https://blockhat.io/' },
-    { name: 'Sponsor 5', imageUrl: '/assets/images/partners/safran.png', link: 'https://www.safran-group.com/' },
+    { name: 'Springer', imageUrl: '/assets/images/partners/springer.webp', link: 'https://www.springer.com/' },
+    { name: 'ENSA', imageUrl: '/assets/images/partners/ensa.webp', link: 'https://ensa-marrakech.uca.ma/' },
+    { name: 'Aminov', imageUrl: '/assets/images/partners/aminov.webp', link: '#'},
+    { name: 'Lartid', imageUrl: '/assets/images/partners/Lartid.jpg', link: 'https://ensa-marrakech.uca.ma/' },
+    { name: 'Blockhat', imageUrl: '/assets/images/partners/Blockhat.png', link: 'https://blockhat.io/' },
+    { name: 'Stellantis', imageUrl: '/assets/images/partners/Stellantis.jpg', link: 'https://www.stellantis.com/fr' },
+    { name: 'UFPR', imageUrl: '/assets/images/partners/UFPR.jpg', link: 'https://internacional.ufpr.br/portal/about-ufpr/' },
+    { name: 'Clarivate', imageUrl: '/assets/images/partners/Clarivate.jpg', link: 'https://clarivate.com/' },
+    { name: 'Scopus', imageUrl: '/assets/images/partners/Scopus.jpg', link: 'https://www.elsevier.com/fr-fr/products/scopus' },
+    { name: 'SDFM', imageUrl: '/assets/images/partners/SDFM.jpg', link: 'https://www.sdfmlogistics.com/' },
   ];
+  
+  allSponsors: Sponsor[] = [];
+
+  ngOnInit() {
+    // Tripler les sponsors pour créer un défilement continu sans saut
+    this.allSponsors = [...this.sponsors, ...this.sponsors, ...this.sponsors];
+  }
 }
